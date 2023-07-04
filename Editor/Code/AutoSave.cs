bc54212c4ce9271b2acbfb91d680507105573449
ProjectMakers <62115521+ProjectMakersDE@users.noreply.github.com>
2023-07-04 12:00:07 +0200

    Cleaner Rewrite

--- Editor/Code/AutoSave.cs
+++ Editor/Code/AutoSave.cs
@@ -11,25 +11,35 @@ namespace PM.Tools
 {
    public class AutoSave : EditorWindow
    {
+      private readonly GUIStyle _guiStyleLabel = new GUIStyle();
+
       private const string LOGColor = "#CC0000";
       private const int LOGSize = 14;
 
-      private readonly GUIStyle _guiStyleLabel = new GUIStyle();
+      private const string EditorPrefPrefix = "PM_AS_";
+      private const string AutoSaveKey = EditorPrefPrefix + "AUTOSAVE";
+      private const string SaveOnPlayKey = EditorPrefPrefix + "SAVEONPLAY";
+      private const string SaveAssetsKey = EditorPrefPrefix + "SAVEASSET";
+      private const string DebugLogKey = EditorPrefPrefix + "DEBUGLOG";
+      private const string SaveIntervalKey = EditorPrefPrefix + "SAVEINTERVAL";
+      private const string BackupKey = EditorPrefPrefix + "BACKUP";
+      private const string BackupPathKey = EditorPrefPrefix + "BACKUPPATH";
+      private const string BackupCountKey = EditorPrefPrefix + "BACKUPCOUNT";
 
-      private bool _autoSave;
-      private bool _saveOnPlay;
-      private bool _saveAssets;
-      private bool _debugLog;
-      private bool _backup;
-      private bool _showBackup;
+      private static bool _autoSave;
+      private static bool _saveOnPlay;
+      private static bool _saveAssets;
+      private static bool _debugLog;
+      private static bool _backup;
+      private static bool _showBackup;
 
-      private DateTime _lastAutoSave = DateTime.Now;
+      private static DateTime _lastAutosave = DateTime.Now;
 
-      private int _saveInterval = 5;
-      private int _saveIntervalSlider = 5;
+      private static int _saveInterval = 5;
+      private static int _saveIntervalSlider = 5;
 
-      private string _backupPath;
-      private int _backupCount;
+      private static string _backupPath;
+      private static int _backupCount;
 
       private Texture2D AsAsset => AssetDatabase.LoadAssetAtPath(Path + "asset.png", typeof(Texture2D)) as Texture2D;
       private Texture2D AsDisable => AssetDatabase.LoadAssetAtPath(Path + "disable.png", typeof(Texture2D)) as Texture2D;
@@ -41,20 +51,20 @@ namespace PM.Tools
       private Texture2D AsBackup => AssetDatabase.LoadAssetAtPath(Path + "backup.png", typeof(Texture2D)) as Texture2D;
       private Texture2D PmLogo => AssetDatabase.LoadAssetAtPath(Path + "logo.png", typeof(Texture2D)) as Texture2D;
 
-      private int BackupCount => EditorPrefs.GetInt("PM_AS_BACKUPCOUNT");
+      private int BackupCount => EditorPrefs.GetInt(BackupCountKey);
 
       private string Path
       {
          get
          {
             string path = AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(this));
-            int index = path.LastIndexOf('/');
-            index = path.LastIndexOf('/', index - 1);
-            return path.Substring(0, index + 1) + "Textures/";
+            path = path[..path.LastIndexOf('/')];
+            path = path[..(path.LastIndexOf('/') + 1)];
+            return path + "Textures/";
          }
       }
 
-      private void AutoSaveOff()
+      private static void AutosaveOff()
       {
          EditorApplication.update -= EditorUpdate;
          EditorApplication.playModeStateChanged -= OnEnterInPlayMode;
@@ -62,18 +72,18 @@ namespace PM.Tools
          Log(0, "OFF !");
       }
 
-      private void AutosaveOn()
+      private static void AutosaveOn()
       {
-         _lastAutoSave = DateTime.Now;
+         _lastAutosave = DateTime.Now;
          EditorApplication.update += EditorUpdate;
          EditorApplication.playModeStateChanged += OnEnterInPlayMode;
          _autoSave = true;
          Log(0, "ON !");
       }
 
-      private void EditorUpdate()
+      private static void EditorUpdate()
       {
-         if (_lastAutoSave.AddMinutes(_saveInterval) > DateTime.Now) return;
+         if (_lastAutosave.AddMinutes(_saveInterval) > DateTime.Now) return;
 
          for (int i = 0; i < EditorSceneManager.sceneCount; i++)
          {
@@ -82,26 +92,38 @@ namespace PM.Tools
             if (scene.isDirty)
             {
                Save();
-               _lastAutoSave = DateTime.Now;
+               _lastAutosave = DateTime.Now;
                break;
             }
          }
       }
 
-      private void LoadSettings()
+      private static void LoadSettings()
       {
-         _autoSave = EditorPrefs.GetBool("PM_AS_AUTOSAVE", true);
-         _saveOnPlay = EditorPrefs.GetBool("PM_AS_SAVEONPLAY", true);
-         _saveAssets = EditorPrefs.GetBool("PM_AS_SAVEASSET", true);
-         _debugLog = EditorPrefs.GetBool("PM_AS_DEBUGLOG", true);
-         _saveInterval = EditorPrefs.GetInt("PM_AS_SAVEINTERVAL", 2);
-         _saveIntervalSlider = EditorPrefs.GetInt("PM_AS_SAVEINTERVAL", 2);
-         _backup = EditorPrefs.GetBool("PM_AS_BACKUP", true);
-         _backupPath = EditorPrefs.GetString("PM_AS_BACKUPPATH", "Backup/Test");
-         _backupCount = EditorPrefs.GetInt("PM_AS_BACKUPCOUNT", 10);
+         _autoSave = EditorPrefs.GetBool(AutoSaveKey, true);
+         _saveOnPlay = EditorPrefs.GetBool(SaveOnPlayKey, true);
+         _saveAssets = EditorPrefs.GetBool(SaveAssetsKey, true);
+         _debugLog = EditorPrefs.GetBool(DebugLogKey, true);
+         _saveInterval = EditorPrefs.GetInt(SaveIntervalKey, 2);
+         _saveIntervalSlider = EditorPrefs.GetInt(SaveIntervalKey, 2);
+         _backup = EditorPrefs.GetBool(BackupKey, true);
+         _backupPath = EditorPrefs.GetString(BackupPathKey, "_project/AutoSave");
+         _backupCount = EditorPrefs.GetInt(BackupCountKey, 10);
       }
 
-      private void OnEnterInPlayMode(PlayModeStateChange state)
+      private static void SaveSettings()
+      {
+         EditorPrefs.SetBool(AutoSaveKey, _autoSave);
+         EditorPrefs.SetBool(SaveOnPlayKey, _saveOnPlay);
+         EditorPrefs.SetBool(SaveAssetsKey, _saveAssets);
+         EditorPrefs.SetBool(DebugLogKey, _debugLog);
+         EditorPrefs.SetBool(BackupKey, _backup);
+         EditorPrefs.SetInt(SaveIntervalKey, _saveInterval);
+         EditorPrefs.SetString(BackupPathKey, _backupPath);
+         EditorPrefs.SetInt(BackupCountKey, _backupCount);
+      }
+
+      private static void OnEnterInPlayMode(PlayModeStateChange state)
       {
          if (_saveOnPlay && !EditorApplication.isPlaying && EditorApplication.isPlayingOrWillChangePlaymode)
             Save();
@@ -113,35 +135,46 @@ namespace PM.Tools
          GetWindow<AutoSave>("AutoSave");
       }
 
-      private void Save()
+      private static void Save()
       {
          Scene activeScene = SceneManager.GetActiveScene();
+         SaveAllDirtyScenes();
 
-         try
+         if (_saveAssets)
+            AssetDatabase.SaveAssets();
+
+         Log(0, $"Scene '{activeScene.name}' has been saved.");
+
+         if (!_backup)
+            return;
+
+         BackupActiveScene(activeScene);
+      }
+
+      private static void SaveAllDirtyScenes()
+      {
+         for (int i = 0; i < EditorSceneManager.sceneCount; i++)
          {
-            for (int i = 0; i < EditorSceneManager.sceneCount; i++)
-            {
-               var scene = EditorSceneManager.GetSceneAt(i);
+            var scene = EditorSceneManager.GetSceneAt(i);
 
+            if (!scene.isDirty) continue;
+
+            try
+            {
                EditorSceneManager.SaveScene(scene);
             }
-
-            if (_saveAssets)
-               AssetDatabase.SaveAssets();
-
-            Log(0, $"{activeScene.name} is saved!");
+            catch (Exception e)
+            {
+               Log(2, $"Error occurred while saving scene '{scene.name}'.\nException: {e}");
+            }
          }
-         catch (Exception e)
-         {
-            Log(2, $"{activeScene.name} is !! NOT !! saved!\nException: {e}");
-         }
+      }
 
-         if (!_backup) return;
-
+      private static void BackupActiveScene(Scene activeScene)
+      {
          var username = SystemInfo.deviceName;
          var curSceneName = activeScene.name;
          var fileName = BackupFileName(curSceneName);
-
          var path = System.IO.Path.Combine("Assets/", _backupPath, username, curSceneName);
          var filePath = System.IO.Path.Combine(path, fileName);
 
@@ -151,79 +184,55 @@ namespace PM.Tools
          try
          {
             EditorSceneManager.SaveScene(activeScene, filePath, true);
-            Log(0, $"Create a backup for - {curSceneName} - Scene!");
+            Log(0, $"Backup created for scene '{curSceneName}'.");
             ClearBackupFolder(path);
          }
          catch (Exception e)
          {
-            Log(2, $"Create a !! NO !! backup for -{curSceneName} - Scene!\nException: {e}");
+            Log(2, $"Error occurred while creating a backup for scene '{curSceneName}'.\nException: {e}");
          }
       }
 
-      private void ClearBackupFolder(string path)
+      private static void ClearBackupFolder(string path)
       {
-         var fileInfo = new DirectoryInfo(path).GetFiles();
+         var fileInfo = new DirectoryInfo(path).GetFiles("*.unity");
 
-         // The *2 because each file has a ".meta" file 
-         if (fileInfo.Length >= _backupCount * 2)
+         if (fileInfo.Length > _backupCount)
          {
-            var dir = fileInfo.OrderByDescending(x => x.LastWriteTime).Skip(_backupCount * 2);
+            var oldestUnityFile = fileInfo.OrderBy(x => x.LastWriteTime).First();
+            var metaFilePath = $"{oldestUnityFile.FullName}.meta";
 
-            foreach (var fi in dir)
-               fi.Delete();
+            oldestUnityFile.Delete();
+
+            if (File.Exists(metaFilePath))
+               File.Delete(metaFilePath);
 
             AssetDatabase.Refresh();
          }
       }
 
-      private string BackupFileName(string curSceneName)
+      private static string BackupFileName(string curSceneName)
       {
-         string formattedTime = DateTime.Now.Ticks.ToString().Substring(3, 7);
-         int number = int.Parse(formattedTime);
-         string filename = $"{curSceneName}_v.{number:#,###}.unity".Replace(",", ".");
+         var timestamp = DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss_fff");
+         var filename = $"{curSceneName} v.{timestamp}.unity";
          return filename;
       }
 
-      private void SaveSettings()
+      private static void Log(int type, string body)
       {
-         EditorPrefs.SetBool("PM_AS_AUTOSAVE", _autoSave);
-         EditorPrefs.SetBool("PM_AS_SAVEONPLAY", _saveOnPlay);
-         EditorPrefs.SetBool("PM_AS_SAVEASSET", _saveAssets);
-         EditorPrefs.SetBool("PM_AS_DEBUGLOG", _debugLog);
-         EditorPrefs.SetBool("PM_AS_BACKUP", _backup);
-         EditorPrefs.SetInt("PM_AS_SAVEINTERVAL", _saveInterval);
-         EditorPrefs.SetString("PM_AS_BACKUPPATH", _backupPath);
-         EditorPrefs.SetInt("PM_AS_BACKUPCOUNT", _backupCount);
-      }
+         if (!_debugLog) return;
 
-      private void ResetSettings()
-      {
-         EditorPrefs.DeleteKey("PM_AS_AUTOSAVE");
-         EditorPrefs.DeleteKey("PM_AS_SAVEONPLAY");
-         EditorPrefs.DeleteKey("PM_AS_SAVEASSET");
-         EditorPrefs.DeleteKey("PM_AS_DEBUGLOG");
-         EditorPrefs.DeleteKey("PM_AS_BACKUP");
-         EditorPrefs.DeleteKey("PM_AS_SAVEINTERVAL");
-         EditorPrefs.DeleteKey("PM_AS_BACKUPPATH");
-         EditorPrefs.DeleteKey("PM_AS_BACKUPCOUNT");
-      }
-
-      private void Log(int type, string body)
-      {
-         if (_debugLog)
+         switch (type)
          {
-            switch (type)
-            {
-               case 1:
-                  Debug.LogWarning($"<color={LOGColor}><size={LOGSize}><b>PM - Autosave: </b></size></color>{body}");
-                  break;
-               case 2:
-                  Debug.LogError($"<color={LOGColor}><size={LOGSize}><b>PM - Autosave: </b></size></color>{body}");
-                  break;
-               default:
-                  Debug.Log($"<color={LOGColor}><size={LOGSize}><b>PM - Autosave: </b></size></color>{body}");
-                  break;
-            }
+            case 1:
+               Debug.LogWarning($"<color={LOGColor}><size={LOGSize}><b>PM - Autosave: </b></size></color>{body}");
+               break;
+            case 2:
+               Debug.LogError($"<color={LOGColor}><size={LOGSize}><b>PM - Autosave: </b></size></color>{body}");
+               break;
+            default:
+               Debug.Log($"<color={LOGColor}><size={LOGSize}><b>PM - Autosave: </b></size></color>{body}");
+               break;
          }
       }
 
@@ -237,6 +246,8 @@ namespace PM.Tools
 
       private void OnGUI()
       {
+         EditorGUI.BeginChangeCheck();
+
          if (_saveInterval != _saveIntervalSlider)
          {
             _saveInterval = _saveIntervalSlider;
@@ -253,48 +264,9 @@ namespace PM.Tools
          EditorGUILayout.LabelField(string.Empty, GUI.skin.horizontalSlider);
          GUILayout.Space(10);
          GUILayout.BeginHorizontal();
-         GUILayout.BeginVertical();
-         GUILayout.BeginHorizontal();
-         GUILayout.Space(10);
-         GUILayout.Label(_debugLog ? AsEnable : AsDisable, GUILayout.MaxHeight(16), GUILayout.MaxWidth(16));
-         GUILayout.EndHorizontal();
-
-         if (GUILayout.Button(new GUIContent(AsInfo, "Create Debug.Log"), GUILayout.MaxHeight(28), GUILayout.MaxWidth(28)))
-         {
-            _debugLog = !_debugLog;
-            Log(0, "Debug.Log = " + _debugLog + " !");
-            SaveSettings();
-         }
-
-         GUILayout.EndVertical();
-         GUILayout.BeginVertical();
-         GUILayout.BeginHorizontal();
-         GUILayout.Space(10);
-         GUILayout.Label(_saveOnPlay ? AsEnable : AsDisable, GUILayout.MaxHeight(16), GUILayout.MaxWidth(16));
-         GUILayout.EndHorizontal();
-
-         if (GUILayout.Button(new GUIContent(AsOnPlay, "Save on Play"), GUILayout.MaxHeight(28), GUILayout.MaxWidth(28)))
-         {
-            _saveOnPlay = !_saveOnPlay;
-            Log(0, "Save on Play = " + _saveOnPlay + " !");
-            SaveSettings();
-         }
-
-         GUILayout.EndVertical();
-         GUILayout.BeginVertical();
-         GUILayout.BeginHorizontal();
-         GUILayout.Space(10);
-         GUILayout.Label(_saveAssets ? AsEnable : AsDisable, GUILayout.MaxHeight(16), GUILayout.MaxWidth(16));
-         GUILayout.EndHorizontal();
-
-         if (GUILayout.Button(new GUIContent(AsAsset, "Save Assets"), GUILayout.MaxHeight(28), GUILayout.MaxWidth(28)))
-         {
-            _saveAssets = !_saveAssets;
-            Log(0, "Save Assets = " + _saveAssets + " !");
-            SaveSettings();
-         }
-
-         GUILayout.EndVertical();
+         DrawButton(ref _debugLog, AsInfo, "Create Debug.Log", "Debug.Log");
+         DrawButton(ref _saveOnPlay, AsOnPlay, "Save on Play", "Save on Play");
+         DrawButton(ref _saveAssets, AsAsset, "Save Assets", "Save Assets");
          GUILayout.FlexibleSpace();
          GUILayout.BeginVertical();
          GUILayout.BeginHorizontal();
@@ -312,23 +284,7 @@ namespace PM.Tools
          GUILayout.EndVertical();
          GUILayout.EndHorizontal();
          GUILayout.EndVertical();
-         GUILayout.BeginVertical();
-         GUILayout.BeginHorizontal();
-         GUILayout.Space(10);
-         GUILayout.Label(_autoSave ? AsEnable : AsDisable, GUILayout.MaxHeight(16), GUILayout.MaxWidth(16));
-         GUILayout.EndHorizontal();
-
-         if (GUILayout.Button(new GUIContent(AsOnOff, "AutoSave ON/OFF"), GUILayout.MaxHeight(28), GUILayout.MaxWidth(28)))
-         {
-            if (_autoSave)
-               AutoSaveOff();
-            else
-               AutosaveOn();
-
-            SaveSettings();
-         }
-
-         GUILayout.EndVertical();
+         DrawButton(ref _autoSave, AsOnOff, "AutoSave ON/OFF", "AutoSave");
          GUILayout.EndHorizontal();
          GUILayout.Space(10);
          GUILayout.BeginHorizontal();
@@ -349,20 +305,7 @@ namespace PM.Tools
          if (_showBackup)
          {
             GUILayout.BeginHorizontal();
-            GUILayout.BeginVertical();
-            GUILayout.BeginHorizontal();
-            GUILayout.Space(10);
-            GUILayout.Label(_backup ? AsEnable : AsDisable, GUILayout.MaxHeight(16), GUILayout.MaxWidth(16));
-            GUILayout.EndHorizontal();
-
-            if (GUILayout.Button(new GUIContent(AsBackup, "Save Project in backupfolder"), GUILayout.MaxHeight(28), GUILayout.MaxWidth(28)))
-            {
-               _backup = !_backup;
-               Log(0, "Backup = " + _backup + " !");
-               SaveSettings();
-            }
-
-            GUILayout.EndVertical();
+            DrawButton(ref _backup, AsBackup, "Save Project in backupfolder", "Backup");
             GUILayout.BeginVertical();
             GUILayout.Space(24);
             GUILayout.BeginHorizontal();
@@ -382,7 +325,7 @@ namespace PM.Tools
 
             Vector2 countSize = GUI.skin.box.CalcSize(new GUIContent(BackupCount.ToString()));
             EditorGUIUtility.labelWidth = 88;
-            EditorPrefs.SetInt("PM_AS_BACKUPCOUNT", EditorGUILayout.IntField("Backup count: ", BackupCount, GUILayout.MinWidth(EditorGUIUtility.labelWidth + countSize.x + 8), GUILayout.MaxWidth(EditorGUIUtility.labelWidth + countSize.x + 8)));
+            EditorPrefs.SetInt(BackupCountKey, EditorGUILayout.IntField("Backup count: ", BackupCount, GUILayout.MinWidth(EditorGUIUtility.labelWidth + countSize.x + 8), GUILayout.MaxWidth(EditorGUIUtility.labelWidth + countSize.x + 8)));
 
             GUILayout.EndHorizontal();
             GUILayout.EndVertical();
@@ -407,6 +350,27 @@ namespace PM.Tools
             Application.OpenURL("https://projectmakers.de");
 
          GUILayout.EndHorizontal();
+
+         if (EditorGUI.EndChangeCheck())
+            SaveSettings();
+      }
+
+      private void DrawButton(ref bool buttonState, Texture buttonInfoText, string buttonText, string logMessage)
+      {
+         GUILayout.BeginVertical();
+         GUILayout.BeginHorizontal();
+         GUILayout.Space(10);
+         GUILayout.Label(buttonState ? AsEnable : AsDisable, GUILayout.MaxHeight(16), GUILayout.MaxWidth(16));
+         GUILayout.EndHorizontal();
+
+         if (GUILayout.Button(new GUIContent(buttonInfoText, buttonText), GUILayout.MaxHeight(28), GUILayout.MaxWidth(28)))
+         {
+            buttonState = !buttonState;
+            Log(0, $"{logMessage} = {buttonState} !");
+            SaveSettings();
+         }
+
+         GUILayout.EndVertical();
       }
    }
 }
